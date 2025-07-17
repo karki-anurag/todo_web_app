309f5ed56ad780bb8b550fe1cabd8214b97fc3d3
anuragkarki <anuragkarki2004@gmail.com>
2025-07-17 13:45:38 +0545

    final mark, edit and delete Function create

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import lists, registration,user_profile,create
+from .views import lists, registration, user_profile, create, mark, edit, delete
 
 
 urlpatterns = [
@@ -7,4 +7,10 @@ urlpatterns = [
     path('create/',create,name = 'create' ),
     path('register/',registration,name='register'),
     path('userprofile/',user_profile,name='userprofile'),
+    path('lists/<int:id>/', mark, name='mark'),
+    path('lists/<int:id>/edit', edit, name='edit'),
+    path('lists/<int:id>/delete', delete, name='delete'),
+
+
+
 ]
--- user/views.py
+++ user/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 
+@login_required
 def lists(request):
     tasks = UserTodo.objects.all()
     return render(request, 'lists.html', {'tasks': tasks})
@@ -23,25 +24,36 @@ def create(request):
         form = TodoForm()
     return render(request, 'create.html', {'form': form})
 
-def edit(request, task_id):
-  tasks = get_object_or_404(UserTodo, pk = task_id, user = request.user)
+@login_required
+def mark(request,id):
+    tasks = UserTodo.objects.get(pk = id, user = request.user)
+    tasks.is_completed = True
+    tasks.save()
+    return redirect('lists')
+
+
+@login_required
+def edit(request, id):
+  tasks = get_object_or_404(UserTodo, pk = id, user = request.user)
   if request.method == 'POST':
     form = TodoForm(request.POST, instance = tasks)
     if form.is_valid():
         todo = form.save(commit=False)
         todo.user = request.user
         todo.save()
+        return redirect('lists')
   else:
        form = TodoForm(instance = tasks ) #we use instance to provide prefill data to the user which is comming from model 'UserTodo'
   return render(request, 'create.html', {'form': form})  
 
 @login_required
-def tweet_delete(request, task_id):
-    tasks = get_object_or_404(UserTodo, pk = task_id, user = request.user)
+def delete(request, id):
+    tasks = UserTodo.objects.get(pk = id, user = request.user)
     if request.method == 'POST':
         tasks.delete()
-        return redirect('tweet_list')
+        return redirect('lists')
     return render(request, 'task_confirm_delete.html', {'tasks': tasks})
+    
 
 def registration(request):
     if request.method == 'POST':
